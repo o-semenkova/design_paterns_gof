561713cab5306d94e02152c1e4e7627035662c62
osem <oksana.semenkova@yahoo.com>
2016-09-18 21:06:30 +0300

    Test code for pattern Observer was fixed

--- src/main/test/observer/WeatherStation.java
+++ src/main/test/observer/WeatherStation.java
@@ -1,5 +1,8 @@
 package observer;
 
+import observer.visual.CurrentState;
+import observer.visual.VisualComponent;
+
 /**
  * Created by osemenkova on 9/16/2016.
  */
@@ -7,6 +10,8 @@ public class WeatherStation {
 
     public static void main(String []args) {
         WeatherData weatherData = new WeatherData("10", "65%");
+
+        VisualComponent vc = new CurrentState(weatherData);
 
         weatherData.setTemperature("15");
         weatherData.setTemperature("16");
